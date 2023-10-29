9a5d597f557c81081d60b8dbc1d2faa7de9d9d08
Nobody <lexuanvu161@gmail.com>
2023-10-30 02:26:17 +0700

    test screen display

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,66 @@
 #include <wx/wx.h>
-#include <wx/bitmap.h>
-#include <wx/dcclient.h>
-#include <wx/statbmp.h>
+#include <wx/dcbuffer.h>
 
 class MyFrame : public wxFrame {
 public:
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size) {
-        wxButton* captureButton = new wxButton(this, wxID_ANY, "Capture and Update Image", wxPoint(10, 10));
-        Bind(wxEVT_BUTTON, &MyFrame::OnCaptureButtonClick, this, captureButton->GetId());
+        : wxFrame(NULL, wxID_ANY, title, pos, size, wxDEFAULT_FRAME_STYLE | wxCLOSE_BOX | wxFULL_REPAINT_ON_RESIZE), capturedImage(1,1) {
+        SetBackgroundStyle(wxBG_STYLE_PAINT);
 
-        // Initial image
-        wxImage::AddHandler(new wxPNGHandler);
-        originalImage = wxImage("download.png", wxBITMAP_TYPE_PNG); // Replace "image.png" with your image file path
-        
-        // Create a wxStaticBitmap control to display the image
-        staticBitmap = new wxStaticBitmap(this, wxID_ANY, wxNullBitmap, wxPoint(10, 50), wxSize(300, 200));
-        UpdateImage(originalImage); // Display the initial image
+        // Set up a timer to continuously update the screen capture
+        timer = new wxTimer(this, wxID_ANY);
+        Bind(wxEVT_TIMER, &MyFrame::OnTimer, this, wxID_ANY);
+        timer->Start(16); // Update screen capture every ~16 milliseconds (approximately 60 FPS)
+
+        Bind(wxEVT_CLOSE_WINDOW, &MyFrame::OnClose, this);
     }
 
-    void OnCaptureButtonClick(wxCommandEvent& event) {
-        // Modify the image (for example, rotate it)
-        wxImage modifiedImage = originalImage.Rotate90(clockwise); // Rotate the image 90 degrees clockwise
-        clockwise = !clockwise; // Toggle the rotation direction for the next click
-        
-        UpdateImage(modifiedImage); // Update the displayed image with the modified image
+    void OnPaint(wxPaintEvent& event) {
+        wxAutoBufferedPaintDC dc(this);
+        dc.DrawBitmap(wxBitmap(capturedImage), 0, 0);
     }
 
-    void UpdateImage(const wxImage& image) {
-        // Create a bitmap from the image
-        wxBitmap bitmap(image);
-        
-        // Set the bitmap to the wxStaticBitmap control to update the displayed image
-        staticBitmap->SetBitmap(bitmap);
+    void OnTimer(wxTimerEvent& event) {
+        // Capture the full screen
+        wxScreenDC screenDC;
+        wxMemoryDC memDC;
+
+        int screenWidth = screenDC.GetSize().GetWidth();
+        int screenHeight = screenDC.GetSize().GetHeight();
+
+        wxBitmap bitmap(screenWidth, screenHeight);
+        memDC.SelectObject(bitmap);
+        memDC.Blit(0, 0, screenWidth, screenHeight, &screenDC, 0, 0);
+        memDC.SelectObject(wxNullBitmap);
+
+        // Convert wxBitmap to wxImage
+        capturedImage = bitmap.ConvertToImage();
+
+        // Trigger a repaint to display the captured screen
+        Refresh();
+    }
+
+    void OnClose(wxCloseEvent& event) {
+        timer->Stop();
+        Destroy();
     }
 
 private:
-    wxImage originalImage;
-    bool clockwise = true; // Rotation direction flag
-    wxStaticBitmap* staticBitmap;
+    wxTimer* timer;
+    wxImage capturedImage;
+
+    wxDECLARE_EVENT_TABLE();
 };
+
+wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_PAINT(MyFrame::OnPaint)
+wxEND_EVENT_TABLE()
 
 class MyApp : public wxApp {
 public:
     virtual bool OnInit() {
-        MyFrame* frame = new MyFrame("Image Display and Modification Example", wxPoint(10, 10), wxSize(800, 600));
+        // Create the main frame
+        MyFrame *frame = new MyFrame("Screen Capture", wxPoint(100, 100), wxSize(800, 600));
         frame->Show(true);
         return true;
     }
