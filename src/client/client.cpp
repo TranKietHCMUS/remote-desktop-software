e2b3e933510a368347fa4cf4cd03a781c2025310
Nobody <lexuanvu161@gmail.com>
2023-11-01 08:48:32 +0700

    setup server/client debug tasks

---
+++ src/client/client.cpp
@@ -0,0 +1,145 @@
+#include <wx/wx.h>
+#include <wx/socket.h>
+
+enum
+{
+    wxID_BUTCONN = 0,
+    wxID_DESC,
+    wxID_SOCKET
+};
+
+class Api : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class Window : public wxFrame
+{
+protected: // Window Controls
+    wxPanel *m_panel;
+    wxButton *m_connect_button;
+    wxTextCtrl *m_log_box;
+
+public:
+    Window(const wxString &title, const wxPoint &pos, const wxSize &size,
+           long style = wxDEFAULT_FRAME_STYLE);
+    void FunConnect(wxCommandEvent &evt);
+    void OnSocketEvent(wxSocketEvent &evt);
+
+private:
+    DECLARE_EVENT_TABLE()
+};
+
+// Event Table
+BEGIN_EVENT_TABLE(Window, wxFrame)
+EVT_BUTTON(wxID_BUTCONN, Window::FunConnect)
+EVT_SOCKET(wxID_SOCKET, Window::OnSocketEvent)
+END_EVENT_TABLE()
+
+void Window::FunConnect(wxCommandEvent &evt)
+{
+    m_log_box->AppendText(_T("Connecting to the server...\n"));
+    m_connect_button->Enable(FALSE);
+
+    // Connecting to the server
+    wxIPV4address adr;
+    adr.Hostname(_T("localhost"));
+    adr.Service(3000);
+
+    // Create the socket
+    wxSocketClient *Socket = new wxSocketClient();
+
+    Socket->SetEventHandler(*this, wxID_SOCKET);
+    Socket->SetNotify(wxSOCKET_CONNECTION_FLAG | wxSOCKET_INPUT_FLAG | wxSOCKET_LOST_FLAG);
+    Socket->Notify(TRUE);
+
+    Socket->Connect(adr, false);
+    m_log_box->AppendText(_T("Connecting...\n"));
+
+    return;
+}
+
+void Window::OnSocketEvent(wxSocketEvent &evt)
+{
+    wxSocketBase *Sock = evt.GetSocket();
+
+    char buffer[10];
+
+    switch (evt.GetSocketEvent())
+    {
+    case wxSOCKET_CONNECTION:
+    {
+        m_log_box->AppendText(_T("OnSocketEvent(wxSOCKET_CONNECTION) Connection successful\n"));
+
+        char mychar = '0';
+
+        for (int i = 0; i < 10; ++i)
+        {
+            buffer[i] = mychar++;
+        }
+
+        Sock->Write(buffer, sizeof(buffer));
+
+        char cstring[256];
+        sprintf(cstring, "%c%c%c%c%c%c%c%c%c%c\n", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5],
+                buffer[6], buffer[7], buffer[8], buffer[9]);
+        m_log_box->AppendText(wxString("    Sent ") + cstring + "\n");
+
+        break;
+    }
+
+    case wxSOCKET_INPUT:
+    {
+        m_log_box->AppendText(_T("OnSocketEvent(wxSOCKET_INPUT)\n"));
+        Sock->Read(buffer, sizeof(buffer));
+
+        char cstring[256];
+        sprintf(cstring, "%c%c%c%c%c%c%c%c%c%c\n", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4], buffer[5],
+                buffer[6], buffer[7], buffer[8], buffer[9]);
+        m_log_box->AppendText(wxString("    received ") + cstring + "\n");
+
+        break;
+    }
+
+    case wxSOCKET_LOST:
+    {
+        m_log_box->AppendText(_T("OnSocketEvent(wxSOCKET_LOST)\n"));
+        Sock->Destroy();
+        m_connect_button->Enable(TRUE);
+        break;
+    }
+
+    default:
+    {
+        m_log_box->AppendText(_T("OnSocketEvent : unknown socket event\n"));
+        break;
+    }
+    }
+}
+
+Window::Window(const wxString &title, const wxPoint &pos, const wxSize &size,
+               long style) : wxFrame(NULL, -1, title, pos, size, style)
+{
+    m_panel = new wxPanel(this, wxID_ANY);
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+
+    m_connect_button = new wxButton(m_panel, wxID_BUTCONN, _T("Connect"), wxPoint(10, 10));
+    sizer->Add(m_connect_button, 0, wxALL, 10);
+
+    m_log_box = new wxTextCtrl(m_panel, wxID_DESC, wxEmptyString, wxPoint(10, 50), wxSize(300, 300), wxTE_MULTILINE | wxTE_BESTWRAP | wxTE_READONLY);
+    sizer->Add(m_log_box, 1, wxEXPAND | wxALL, 10);
+    m_panel->SetSizer(sizer);
+
+    m_log_box->SetValue("Welcome in my SocketDemo: Client\nClient Ready!\n\n");
+}
+
+bool Api::OnInit()
+{
+    Window *MainWindow = new Window(_T("SocketDemo: Client"), wxDefaultPosition, wxSize(500, 500));
+    MainWindow->Centre();
+    MainWindow->Show(TRUE);
+    return TRUE;
+}
+
+IMPLEMENT_APP(Api)
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <wx/wx.h>
-#include <wx/dcbuffer.h>
 
 class MyFrame : public wxFrame {
 public:
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size, wxDEFAULT_FRAME_STYLE | wxCLOSE_BOX | wxFULL_REPAINT_ON_RESIZE), capturedImage(1,1) {
+        : wxFrame(NULL, wxID_ANY, title, pos, size,
+                  wxDEFAULT_FRAME_STYLE | wxCLOSE_BOX | wxFULL_REPAINT_ON_RESIZE),
+          capturedImage(1, 1) { 
         SetBackgroundStyle(wxBG_STYLE_PAINT);
 
-        // Set up a timer to continuously update the screen capture
+        // Set up the window components, bind events, etc.
         timer = new wxTimer(this, wxID_ANY);
         Bind(wxEVT_TIMER, &MyFrame::OnTimer, this, wxID_ANY);
         timer->Start(16); // Update screen capture every ~16 milliseconds (approximately 60 FPS)
@@ -15,13 +16,7 @@ public:
         Bind(wxEVT_CLOSE_WINDOW, &MyFrame::OnClose, this);
     }
 
-    void OnPaint(wxPaintEvent& event) {
-        wxAutoBufferedPaintDC dc(this);
-        dc.DrawBitmap(wxBitmap(capturedImage), 0, 0);
-    }
-
     void OnTimer(wxTimerEvent& event) {
-        // Capture the full screen
         wxScreenDC screenDC;
         wxMemoryDC memDC;
 
@@ -31,13 +26,14 @@ public:
         wxBitmap bitmap(screenWidth, screenHeight);
         memDC.SelectObject(bitmap);
         memDC.Blit(0, 0, screenWidth, screenHeight, &screenDC, 0, 0);
-        memDC.SelectObject(wxNullBitmap);
 
-        // Convert wxBitmap to wxImage
         capturedImage = bitmap.ConvertToImage();
-
-        // Trigger a repaint to display the captured screen
         Refresh();
+    }
+
+    void OnPaint(wxPaintEvent& event) {
+        wxPaintDC dc(this); 
+        dc.DrawBitmap(wxBitmap(capturedImage), 0, 0, false);
     }
 
     void OnClose(wxCloseEvent& event) {
@@ -59,7 +55,6 @@ wxEND_EVENT_TABLE()
 class MyApp : public wxApp {
 public:
     virtual bool OnInit() {
-        // Create the main frame
         MyFrame *frame = new MyFrame("Screen Capture", wxPoint(100, 100), wxSize(800, 600));
         frame->Show(true);
         return true;
